c0637c41707937c46d7549f0df7c3b87e47d3c0e
Momo4894 <128304696+Momo4894@users.noreply.github.com>
2024-04-25 08:31:48 +0200

    First Test

--- src/main/java/httpserver/server/Response.java
+++ src/main/java/httpserver/server/Response.java
@@ -33,4 +33,6 @@ public class Response {
                 "\r\n" +
                 this.content;
     }
+
+    public int getStatus() { return status; }
 }
--- src/test/java/UserServiceTest.java
+++ src/test/java/UserServiceTest.java
@@ -1,18 +1,43 @@
+import httpserver.server.Request;
+import httpserver.server.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import seb.controller.UserController;
 import seb.dal.repository.UserRepository;
 import seb.model.User;
 import seb.service.user.UserService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
     @Mock
     private UserRepository userRepository;
 
-    private UserService userService;
+
+    @InjectMocks
+    private UserController userController;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testRegisterUser_Success() throws Exception {
+        Request request = new Request();
+        request.setBody("{\"Username\":\"Bert\", \"Password\":\"myPassword\"}");
+
+        Response response = userController.addUser(request);
+
+        assertEquals(201, response.getStatus());
+    }
 
 
 }
